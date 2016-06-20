4b05953d79a591f857204dd5e3b0645c6ef3fc86
Mehmet Arda Ozdemir <mehmetardaoz@gmail.com>
2016-06-20 17:44:53 +0300

    initial revision

---
+++ mainwindow.cpp
@@ -0,0 +1,46 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "QGraphicsScene"
+#include "QFileDialog"
+
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::on_openButton_clicked()
+{
+    QGraphicsScene *scene;
+    QString imagePath = QFileDialog::getOpenFileName(
+            this,
+            tr("Open File"),
+                "/",
+            tr("JPEG (*.jpg *.jpeg);;PNG (*.png)" )
+    );
+
+    imageObject = new QImage();
+    imageObject->load(imagePath);
+
+    image = QPixmap::fromImage(*imageObject);
+
+    scene = new QGraphicsScene(this);
+    scene->addPixmap(image);
+    scene->setSceneRect(image.rect());
+    ui->graphicsView->setScene(scene);
+    ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
+
+
+}
+
+
+
+
