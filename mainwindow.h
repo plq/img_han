fec54d428d2513caaa74e845e0dfdfb26f73b3a0
mehmetardaoz <mehmetardaoz@gmail.com>
2016-06-28 10:19:09 +0300

    scroll bar set for scaling.

--- mainwindow.h
+++ mainwindow.h
@@ -2,11 +2,12 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-
+#include <QGraphicsPixmapItem>
 
 QT_FORWARD_DECLARE_CLASS(QGraphicsScene)
 
 class QGraphicsScene;
+
 
 
 namespace Ui {
@@ -21,14 +22,12 @@ public:
 
 private slots:
     void on_openButton_clicked();
-
     void on_sld_quality_valueChanged(int value);
     void on_sld_scale_valueChanged(int value);
     void on_saveButton_clicked();
 
 private:
     void show_pixmap();
-
     Ui::MainWindow *ui;
     QPixmap m_pixmap;
     QImage  *m_image;
