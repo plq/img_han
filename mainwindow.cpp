45ffd6a04bf2faab6ce1061f948c72b4289c1487
mehmetardaoz <mehmetardaoz@gmail.com>
2016-06-28 10:16:09 +0300

    focus problem was solved.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,10 +1,18 @@
-
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
 #include <QFileDialog>
 #include <QGraphicsScene>
-
+#include <QImage>
+#include <QGraphicsItem>
+#include <QGraphicsPixmapItem>
+#include <QAbstractScrollArea>
+#include <QScrollBar>
+#include <QDebug>
+#include <QTextEdit>
+#include <QPainter>
+#include <QGraphicsPixmapItem>
+#include <QGraphicsAnchor>
+#include <QGraphicsView>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -23,31 +31,30 @@ MainWindow::~MainWindow()
 void MainWindow::on_openButton_clicked()
 {
     QString imagePath = QFileDialog::getOpenFileName(
-            this, tr("Open File"), QDir::rootPath(),
-            tr("JPEG (*.jpg *.jpeg);;PNG (*.png)"));
+            this, tr("Open File"), /*QDir::rootPath()*/ "/home/arda/Masaüstü",
+            tr("JPEG (*.jpg *.jpeg);;PNG (*.png);;BMP (*.bmp)"));
 
     m_image = new QImage();
     m_image->load(imagePath);\
 
     m_pixmap = QPixmap::fromImage(*m_image);
-
     show_pixmap();
 
     ui->graphicsView->setScene(m_scene);
     ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
+
 }
 
 
 void MainWindow::on_saveButton_clicked()
 {
     QString imagePath = QFileDialog::getSaveFileName(
-            this,tr("Save File"),QDir::rootPath(),
-            tr("JPEG (*.jpg *.jpeg);;PNG (*.png)" ));
+            this,tr("Save File"),/*QDir::rootPath()*/ "/home/arda/Masaüstü",
+            tr("JPEG (*.jpg *.jpeg);;PNG (*.png);;BMP (*.bmp)"));
 
     *m_image = m_pixmap.toImage();
      m_image->save(imagePath);
 }
-
 
 
 void MainWindow::show_pixmap()
@@ -63,12 +70,15 @@ void MainWindow::show_pixmap()
     m_scene->setSceneRect(m_pixmap.rect());
 }
 
+
 void MainWindow::on_sld_quality_valueChanged(int value) {
 
 }
 
+
 void MainWindow::on_sld_scale_valueChanged(int value) {
 
+    ui->graphicsView->setTransformationAnchor(QGraphicsView::AnchorViewCenter);
     int w = m_image->width();
     int h = m_image->height();
 
@@ -78,5 +88,13 @@ void MainWindow::on_sld_scale_valueChanged(int value) {
     m_pixmap = QPixmap::fromImage(
                 m_image->scaled(new_w, new_h, Qt::KeepAspectRatio, Qt::FastTransformation));
 
-    show_pixmap();
+
+    if(value > 80)
+    ui->graphicsView->scale(value/100.0,value/100.0);
+    else if(value < 80)
+    ui->graphicsView->scale(1/(value/100.0),1/(value/100.0));
+
+
+
 }
+
