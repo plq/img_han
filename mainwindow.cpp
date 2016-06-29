7552e9c8d355cbb29b48d7b9621640e7a99e9f7a
mehmetardaoz <mehmetardaoz@gmail.com>
2016-06-30 00:38:23 +0300

    show image resolution. fixed #5

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -7,11 +7,14 @@
 #include <QDebug>
 #include <QGraphicsView>
 #include <QBuffer>
+#include <QLabel>
+#include <QImageReader>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     m_scene(nullptr),
     m_image(nullptr),
+    m_orig_size(0),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
@@ -29,14 +32,16 @@ void MainWindow::on_openButton_clicked()
             tr("JPEG (*.jpg *.jpeg);;PNG (*.png);;BMP (*.bmp);;WEBP (*.webp)"));
 
     m_image = new QImage();
-    m_image->load(imagePath);\
+    m_image->load(imagePath);
+
+    ui->lbl_width->setText(QString::number(m_image->width()));
+    ui->lbl_height->setText(QString::number(m_image->height()));
 
     m_pixmap = QPixmap::fromImage(*m_image);
     show_pixmap();
 
     ui->graphicsView->setScene(m_scene);
     ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
-
 }
 
 
@@ -67,16 +72,25 @@ void MainWindow::show_pixmap()
 
 void MainWindow::on_sld_quality_valueChanged(int value)
 {
+
     QByteArray ba;
     QBuffer buffer(&ba);
     buffer.open(QIODevice::WriteOnly);
     m_image->save(&buffer,"WEBP",value);
+
+
 
     QImage image;
     image.loadFromData(ba);
     m_pixmap = QPixmap::fromImage(image);
 
     show_pixmap();
+
+    ui->lbl_quality->setText(QString::number(value));
+
+
+
+
 }
 
 
@@ -88,13 +102,16 @@ void MainWindow::on_sld_scale_valueChanged(int value) {
     int new_w = (w * value)/100;
     int new_h = (h * value)/100;
 
+    ui->lbl_width->setText(QString::number(new_w));
+    ui->lbl_height->setText(QString::number(new_h));
+
     m_pixmap = QPixmap::fromImage(
                 m_image->scaled(new_w, new_h, Qt::KeepAspectRatio, Qt::FastTransformation));
 
     show_pixmap();
+    ui->lbl_scale->setText(QString::number(value));
 
-
-    ui->graphicsView->scale(value/100.0,value/100.0);
-
-
+ //   ui->graphicsView->scale(value/100.0,value/100.0);
 }
+
+
