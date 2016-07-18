fef3a810277fcb27a40fff8bec9f45c0cdb6acaa
mehmetardaoz <mehmetardaoz@gmail.com>
2016-07-18 17:01:34 +0300

    horizontal wheelEvent ignored.

--- wheeledgraphicsview.cpp
+++ wheeledgraphicsview.cpp
@@ -3,25 +3,25 @@
 
 #include <QGraphicsView>
 #include <QWheelEvent>
+#include <QScrollBar>
 
+const double WheeledGraphicsView::ZOOM_RATIO_PER_WHEEL_TICK = 1.15;
 
 
 WheeledGraphicsView::WheeledGraphicsView(QWidget *parent) : QGraphicsView(parent){
-}
 
+}
 
 void WheeledGraphicsView::wheelEvent(QWheelEvent *event){
-
-        setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
-        double scaleFactor = 1.15;
+    if(event->orientation() == Qt::Horizontal) {
+        event->ignore();
+    }
+    else {
         if(event->delta() > 0) {
-            emit scale(scaleFactor, scaleFactor);
+            scale(ZOOM_RATIO_PER_WHEEL_TICK, ZOOM_RATIO_PER_WHEEL_TICK);
         }
         else {
-            emit scale(1.0 / scaleFactor, 1.0 / scaleFactor);
+            scale(1.0 / ZOOM_RATIO_PER_WHEEL_TICK, 1.0 / ZOOM_RATIO_PER_WHEEL_TICK);
         }
+    }
 }
-
-
-
-
