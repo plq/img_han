a499b70a1e49af755b5fb0ab7b385f4daa07b3cd
mehmetardaoz <mehmetardaoz@gmail.com>
2016-07-19 10:53:30 +0300

    thread safety added.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,8 +1,12 @@
-
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "wheeledgraphicsview.h"
 
 #include <QtConcurrent/QtConcurrentRun>
+
+#include <mutex>
+#include <string>
+#include <iostream>
 
 #include <QEvent>
 #include <QImage>
@@ -13,6 +17,7 @@
 #include <QShortcut>
 #include <QScrollBar>
 #include <QWheelEvent>
+#include <QMouseEvent>
 #include <QFileDialog>
 #include <QMessageBox>
 #include <QImageReader>
@@ -72,19 +77,20 @@ void MainWindow::on_btn_open_clicked() {
 
     m_orig_size = QFileInfo(imagePath).size();
 
-    ui->lbl_size->setText(QString::number(m_orig_size/1024.00));
+    m_current_scale = 100;
+    m_current_size = m_orig_size;
 
-    ui->lbl_width->setText(QString::number(m_image->width()));
-    ui->lbl_height->setText(QString::number(m_image->height()));
+    qDebug() << "w" << m_image->width() << "h" << m_image->height();
 
-    ui->lbl_running->setStyleSheet("QLabel { background-color : green; color : black; }");
+    ui->lbl_dimensions->setText(
+                QString("%1x%2").arg(m_image->width())
+                                .arg(m_image->height()));
 
     m_pixmap = QPixmap::fromImage(*m_image);
     show_pixmap();
 
     ui->graphicsView->setScene(m_scene);
     ui->graphicsView->setDragMode(QGraphicsView::ScrollHandDrag);
-
 }
 
 void MainWindow::on_btn_save_clicked() {
@@ -109,65 +115,19 @@ void MainWindow::show_pixmap() {
     m_scene->addPixmap(m_pixmap);
     m_scene->setSceneRect(m_pixmap.rect());
 
-    ui->graphicsView->viewport()->installEventFilter(this);
-
-    ui->lbl_running->setStyleSheet("QLabel { background-color : green; color : black; }");
-
-    m_processing = false;
-}
-
-void MainWindow::reprocess_image(int scale, int quality) {
-    if (m_processing) {
-        return;
-    }
-
-    ui->lbl_running->setStyleSheet("QLabel { background-color : red; color : black; }");
-
-    QtConcurrent::run(this, &MainWindow::reprocess_image_impl, scale, quality);
-}
-
-void MainWindow::reprocess_image_impl(int scale, int quality) {
-    rescale_image(scale);
-    requality_image(quality);
-
-    QMetaObject::invokeMethod(this, "show_pixmap");
-}
-
-void MainWindow::rescale_image(int scale) {
-    int w = m_image->width();
-    int h = m_image->height();
-    int new_w = (w * scale)/100;
-    int new_h = (h * scale)/100;
-
-    ui->lbl_width->setText(QString::number(new_w));
-    ui->lbl_height->setText(QString::number(new_h));
-
-    m_pixmap = QPixmap::fromImage(
-                m_image->scaled(new_w, new_h, Qt::KeepAspectRatio, Qt::FastTransformation));
-
-    ui->lbl_scale->setText(QString::number(scale));
-}
-
-void MainWindow::requality_image(int quality) {
-    QByteArray ba;
-    QBuffer buffer(&ba);
-    buffer.open(QIODevice::WriteOnly);
-    m_pixmap.save(&buffer, "WEBP", quality);
-
-    auto l_size_b = buffer.size();
-    double l_size_kb = buffer.size() / 1024.00;
-    ui->lbl_size->setText(QString::number(l_size_kb));
-
-    QImage image;
-    image.loadFromData(ba);
-    m_pixmap = QPixmap::fromImage(image);
+    ui->lbl_busy->setStyleSheet("QLabel { background-color : green; color : black; }");
+    ui->lbl_size->setText(QString::number(m_orig_size/1024.00));
+    ui->lbl_dimensions->setText(
+                QString("%1x%2").arg(new_w)
+                                .arg(new_h));
+    ui->lbl_scale->setText(QString::number(m_current_scale));
 
     auto sld_value_quality = ui->sld_quality->value();
+    double comp_p = 100.0 * m_current_size / m_orig_size;
+
     ui->lbl_quality->setText(QString::number(sld_value_quality));
 
-    double comp_p = 100.0 * l_size_b / m_orig_size;
-
-    if(comp_p>100) {
+    if(comp_p > 100) {
         ui->lbl_compression->setText(QString::number(comp_p));
         QLabel* m_label = ui->lbl_size;
         m_label->setStyleSheet("QLabel { background-color : red; color : black; }");
@@ -177,6 +137,61 @@ void MainWindow::requality_image(int quality) {
         QLabel* m_label = ui->lbl_size;
         m_label->setStyleSheet("QLabel { background-color : rgba(0,0,0,0); color : black; }");
     }
+
+    double l_size_kb = m_current_size / 1024.00;
+    ui->lbl_size->setText(QString::number(l_size_kb));
+
+    std::lock_guard<std::mutex> guard(m_mutex);
+    m_processing = false;
+}
+
+void MainWindow::reprocess_image(int scale, int quality) {
+
+    std::lock_guard<std::mutex> guard(m_mutex);
+    if (m_processing) {
+        return;
+    }
+
+    ui->lbl_busy->setStyleSheet("QLabel { background-color : red; color : black; }");
+
+    QtConcurrent::run(this, &MainWindow::reprocess_image_impl, scale, quality);
+}
+
+void MainWindow::reprocess_image_impl(int scale, int quality) {
+
+    std::lock_guard<std::mutex> guard(m_mutex);
+    rescale_image(scale);
+    requality_image(quality);
+
+    QMetaObject::invokeMethod(this, "show_pixmap");
+}
+
+void MainWindow::rescale_image(int scale) {
+    int w = m_image->width();
+    int h = m_image->height();
+    new_w = (w * scale)/100;
+    new_h = (h * scale)/100;
+
+    qDebug() << "a" << new_w << "b" << new_h ;
+    m_current_scale = scale;
+
+    m_pixmap = QPixmap::fromImage(
+                m_image->scaled(new_w, new_h, Qt::KeepAspectRatio, Qt::FastTransformation));
+}
+
+void MainWindow::requality_image(int quality) {
+    QByteArray ba;
+    QBuffer buffer(&ba);
+    buffer.open(QIODevice::WriteOnly);
+    m_pixmap.save(&buffer, "WEBP", quality);
+
+    m_current_size = buffer.size();
+
+    qDebug() << "x = " << buffer.size();
+
+    QImage image;
+    image.loadFromData(ba);
+    m_pixmap = QPixmap::fromImage(image);
 }
 
 void MainWindow::on_sld_quality_valueChanged(int value) {
@@ -187,21 +202,7 @@ void MainWindow::on_sld_scale_valueChanged(int scale) {
     reprocess_image(scale, ui->sld_quality->value());
 }
 
-void MainWindow::wheelEvent(QWheelEvent *event){
-    QMainWindow::wheelEvent(event);
 
-    ui->graphicsView->setTransformationAnchor(QGraphicsView::AnchorUnderMouse);
-
-    double scaleFactor = 1.15;
-    if(event->delta() > 0) {
-        // Zoom in
-        ui->graphicsView-> scale(scaleFactor, scaleFactor);
-    }
-    else {
-        // Zooming out
-        ui->graphicsView->scale(1.0 / scaleFactor, 1.0 / scaleFactor);
-    }
-}
 
 void MainWindow::on_btn_zoomin_clicked(){
     ui->graphicsView->setTransformationAnchor(QGraphicsView::AnchorViewCenter);
@@ -215,19 +216,10 @@ void MainWindow::on_btn_zoomout_clicked(){
     ui->graphicsView->scale(1.0 / scaleFactor, 1.0 / scaleFactor);
 }
 
-bool MainWindow::eventFilter(QObject *object, QEvent *event){
-    if (object == ui->graphicsView->viewport() && event->type() == QEvent::Wheel){
-        return true;
-    }
-    return false;
-}
-
-void MainWindow::on_btn_rotate_right_clicked()
-{
+void MainWindow::on_btn_rotate_right_clicked(){
     ui->graphicsView->rotate(90);
 }
 
-void MainWindow::on_btn_rotate_left_clicked()
-{
+void MainWindow::on_btn_rotate_left_clicked(){
     ui->graphicsView->rotate(-90);
 }
